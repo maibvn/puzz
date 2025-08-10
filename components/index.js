2ceef5b2bd8fb4d0e9da5663e04b53d6a96bbbd9
maibuivn <vjtkhongcanh93@gmail.com>
2025-08-10 11:44:51 +0700

    Update component exports with Header and new utilities

--- components/index.js
+++ components/index.js
@@ -1,14 +1,17 @@
 // Centralized UI components export
 export { default as Button, ButtonPresets } from "./Button";
+export { default as Header } from "./Header";
 export {
   default as Icon,
   IconPresets,
   getRarityIcon,
   getRarityIconName,
 } from "./Icon";
+export { default as LightSweepEffect } from "./LightSweepEffect";
 export { default as PauseModal } from "./PauseModal";
 export { default as PuzzleBoard } from "./PuzzleBoard";
 export { default as SettingsModal } from "./SettingsModal";
+export { default as SoundToggleRow } from "./SoundToggleRow";
 export { default as Tile } from "./Tile";
 export { default as Timer } from "./Timer";
 export { default as WinScreen } from "./WinScreen";
